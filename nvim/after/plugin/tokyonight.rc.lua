4f0439c334e88ec9b053d408633b563e0f3d4f4a
kamesuke42 <kamesuke42@kamesuke42.com>
2022-11-01 08:27:38 +0900

    override tokyonight

--- nvim/after/plugin/tokyonight.rc.lua
+++ nvim/after/plugin/tokyonight.rc.lua
@@ -1,1 +1,13 @@
+local status, tokyonight = pcall(require, "tokyonight")
+if not status then
+	return
+end
+
+tokyonight.setup({
+	styles = {
+		comments = { italic = false },
+		keywords = { italic = false },
+	},
+})
+
 vim.cmd([[colorscheme tokyonight-night]])
