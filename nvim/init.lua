d70cfc1396ddf716c53e920df04abee11a920828
kamesuke42 <kamesuke42@kamesuke42.com>
2022-11-01 09:14:32 +0900

    update init lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
-require('base')
-require('plugins')
-require('maps')
+--
+-- kamesuke42's dotfiles
+--
+-- author: kamesuke42 <kamesuke42@kamesuke42.com>
+-- repository: https://github.com/kamesuke42/dotfiles
+--
+
+require("base")
+require("plugins")
+require("maps")
