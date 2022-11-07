38cb7f37f5c731f6a996e2e895a09c3fc7d3c398
kamesuke42 <kamesuke42@kamesuke42.com>
2022-11-07 22:09:22 +0900

    Update

--- nvim/after/plugin/mason.rc.lua
+++ nvim/after/plugin/mason.rc.lua
@@ -1,41 +1,42 @@
-local mason_status, mason = pcall(require, 'mason')
+local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
-local mason_lspconfig_status, mason_lspconfig = pcall(require, 'mason-lspconfig')
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
-local mason_null_ls_status, mason_null_ls = pcall(require, 'mason-null-ls')
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  return
+	return
 end
 
 mason.setup()
 
 mason_lspconfig.setup({
-  ensure_installed = {
-    'clangd',
-    'cssls',
-    'gopls',
-    'html',
-    'intelephense',
-    'tailwindcss',
-    'rust_analyzer',
-    'pyright',
-    'sumneko_lua',
-    'tsserver',
-    'zls',
-  }
+	ensure_installed = {
+		"astro-language-server",
+		"clangd",
+		"cssls",
+		"gopls",
+		"html",
+		"intelephense",
+		"tailwindcss",
+		"rust_analyzer",
+		"pyright",
+		"sumneko_lua",
+		"tsserver",
+		"zls",
+	},
 })
 
 mason_null_ls.setup({
-  ensure_installed = {
-    'black',
-    'eslint_d',
-    'prettier',
-    'stylua',
-  }
+	ensure_installed = {
+		"black",
+		"eslint_d",
+		"prettier",
+		"stylua",
+	},
 })
