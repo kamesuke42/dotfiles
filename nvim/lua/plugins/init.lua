36b66f6c51f9a1e82f74bd43878c7a03388dafca
kamesuke42 <4amesuke42@gmail.com>
2023-07-25 09:28:45 +0900

    Neovim: hop to flash

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -316,15 +316,6 @@ return {
     "vim-denops/denops.vim"
   },
   {
-    "phaazon/hop.nvim",
-    config = function()
-      require("hop").setup()
-    end,
-    keys = {
-      { "<leader>j", "<cmd>HopChar1<cr>", desc = "Hop Char1" },
-    },
-  },
-  {
     'numToStr/Comment.nvim',
     config = function()
       require('Comment').setup()
@@ -364,5 +355,17 @@ return {
   },
   {
     "lambdalisue/gin.vim"
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    }
   }
 }
