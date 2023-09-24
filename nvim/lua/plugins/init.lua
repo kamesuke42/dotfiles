78056a4de3afac2c7d73e3ee68960618bc4a0212
kamesuke42 <4amesuke42@gmail.com>
2023-09-24 18:26:58 +0900

    Add lsp_lines.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -439,4 +439,11 @@ return {
 			})
 		end,
 	},
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			require("lsp_lines").setup()
+			vim.diagnostic.config({ virtual_text = false })
+		end,
+	},
 }
