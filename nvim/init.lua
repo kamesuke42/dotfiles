1ccabadfcd2439d1728abd4e977b4d5b61a24507
kamesuke42 <4amesuke42@gmail.com>
2023-09-24 18:31:09 +0900

    Lua format

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,39 +18,39 @@ vim.keymap.set("n", "<space>co", ":copen<cr>", { silent = true })
 vim.keymap.set("n", "<space>cl", ":cclose<cr>", { silent = true })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua", "html", "json", "javascript", "typescript", "typescriptreact" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.shiftwidth = 2
-  end
+	pattern = { "lua", "html", "json", "javascript", "typescript", "typescriptreact" },
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  install = { colorscheme = { "tokyonight" } },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-        "netrwPlugin",
-      },
-    }
-  },
+	install = { colorscheme = { "tokyonight" } },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+				"netrwPlugin",
+			},
+		},
+	},
 })
