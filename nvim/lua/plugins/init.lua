e6220d06d6ea649c25ea98090aedc44650fc129d
kamesuke42 <4amesuke42@gmail.com>
2023-11-20 22:48:29 +0900

    nvim: add hrsh7th/nvim-insx

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -446,4 +446,26 @@ return {
 			vim.diagnostic.config({ virtual_text = false })
 		end,
 	},
+	{
+		"hrsh7th/nvim-insx",
+		config = function()
+			require("insx.preset.standard").setup()
+			local insx = require("insx")
+			insx.add(
+				"'",
+				insx.with(
+					require("insx.recipe.auto_pair")({
+						open = "'",
+						close = "'",
+					}),
+					{
+						insx.with.in_string(false),
+						insx.with.in_comment(false),
+						insx.with.nomatch([[\\\%#]]),
+						insx.with.nomatch([[\a\%#]]),
+					}
+				)
+			)
+		end,
+	},
 }
