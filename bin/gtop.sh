afc22590b21717bb544da9adfbac6245d285e188
kamesuke42 <4amesuke42@gmail.com>
2023-12-01 17:24:26 +0900

    Delete all files for 2024

--- bin/gtop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd $(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
--- nvim/after/ftplugin/kotlin.lua
+++
@@ -1,4 +0,0 @@
-vim.o.expandtab = false
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
