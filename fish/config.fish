8c58404669ac9df88db2359909dac78a75796a38
kamesuke42 <4amesuke42@gmail.com>
2024-01-07 20:35:09 +0900

    fish: update

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,13 @@ if status is-interactive
     set -gx PATH "$HOME/.cargo/bin" $PATH;
     set -gx DENO_INSTALL $HOME/.deno
     set -gx PATH $PATH $DENO_INSTALL/bin
+    set -gx VOLTA_HOME $HOME/.volta
+    set -gx PATH $PATH $VOLTA_HOME/bin
+
+    function fish_prompt -d "Write out the prompt"
+        printf '%s%s%s 🐢 ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    end
+
+    set -x PATH $HOME/.rbenv/bin $PATH
+    status --is-interactive; and source (rbenv init -|psub)
 end
