6580a3080510407ac8bad465fbe73164b8e819d7
kamesuke42 <4amesuke42@gmail.com>
2024-01-07 20:34:40 +0900

    fish: Fix cargo path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    source $HOME/.cargo/env
+    set -gx PATH "$HOME/.cargo/bin" $PATH;
     set -gx DENO_INSTALL $HOME/.deno
     set -gx PATH $PATH $DENO_INSTALL/bin
 end
